f7cd76a521098e6910bac9b5732770feca89e481
JaninduMunasinghe <munasin-ps20057@stu.kln.ac.lk>
2025-03-18 20:09:13 +0530

    added kubernetes deployment configurations

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ from flask_sqlalchemy import SQLAlchemy
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5009, debug=True)
